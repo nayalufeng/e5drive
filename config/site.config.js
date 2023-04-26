fa4a3c06751a34fd9c649133a026e78b56154d6c
naya2016 <63491927+naya2016@users.noreply.github.com>
2023-04-26 10:21:57 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,8 +38,10 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://naya2016.github.io/" target="_blank" rel="noopener noreferrer">纳雅2016</a>. Made with ❤ by 纳雅2016.',
+    'Powered by <a href="https://naya2016.github.io/" target="_blank" rel="noopener noreferrer">纳雅2016</a>. Made with ❤ by 纳雅2016.','<style>
+body{background-image:url(https://onedrive.lizonglin.site/api/raw/?path=/%E5%A5%B9/QQ%E5%9B%BE%E7%89%8720200720192616.jpg);background-repeat:no-repeat;background-position:center}
 
+ </style>'
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: ['/三年模拟', '/保密柜'],
