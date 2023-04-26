adaa70c5ac3a721cd146bbe4696a98e1fe09735e
naya2016 <63491927+naya2016@users.noreply.github.com>
2023-04-26 16:19:14 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
     '<style>body{background-image=url(https://e5.lizonglin.site/api/raw/?path=/%E4%B8%89%E5%B9%B4%E6%A8%A1%E6%8B%9F/%E9%BB%91%E6%96%99/%E7%A7%83%E5%B4%BD%E4%B8%8D%E6%98%AF%E5%9D%8F%E5%A5%B3%E4%BA%BA/%E7%A7%83%E5%B4%BD.jpg&odpt=2899ace7b972b6807a53e70a58f282c41d7b689cbce5e1b2adb1772c904a3b08);background-repeat=no-repeat;background-position=center}</style>',
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/三年模拟', '/保密柜','/从入门到入狱'],
+  protectedRoutes: ['/三年模拟', '/保密柜', '/从入门到入狱'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:247015815@qq.com',
